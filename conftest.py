b1fd76eb119583f444a96cf0ffd3e80f5bf91e05
LesnerNL <magometeleev@gmail.com>
2024-08-16 23:41:45 +0300

    Add files via upload

---
+++ conftest.py
@@ -0,0 +1,9 @@
+# conftest.py
+import pytest
+from selenium import webdriver
+
+@pytest.fixture(scope="function")
+def browser():
+    browser = webdriver.Chrome()
+    yield browser
+    browser.quit()
